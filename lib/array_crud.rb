bcbde276b34bb2bf1bffc0b6288f9b58fdee0372
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-08 22:32:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,8 @@ def create_an_empty_array
   Array.new
 end
 
-def create_an_array(x)
-  array = ["#{x}", "#{x}", "#{x}", "#{x}"]
+def create_an_array
+  array = [" ", " ", " ", " "]
 end
 
 def add_element_to_end_of_array(array, element)
